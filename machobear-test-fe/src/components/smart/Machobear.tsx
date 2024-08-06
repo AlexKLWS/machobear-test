05a3cb3f0e9e01db09571e9781e38da7be3585e5
Alex Korzh <alexkorzh7@gmail.com>
2024-08-06 14:17:53 -0700

    Add machobear UI element

---
+++ machobear-test-fe/src/components/smart/Machobear.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export const Machobear = () => {
+  const [positionOffset, setPositionOffset] = useState({
+    right: 0,
+    top: 0,
+  });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setPositionOffset({ right: e.pageX / 80, top: e.pageY / 100 });
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => document.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  return (
+    <div className="absolute h-full w-full pointer-events-none">
+      <Image
+        src={`/bearchute-nq8.png`}
+        alt={"Bear with a parachute"}
+        width="86"
+        height="86"
+        className="absolute"
+        style={{
+          right: 20 + positionOffset.right,
+          top: 10 - positionOffset.top,
+        }}
+      />
+    </div>
+  );
+};
