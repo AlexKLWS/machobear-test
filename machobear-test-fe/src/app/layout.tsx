dfcde738f0b5fdffffe315a96fac847c2b294746
Alex Korzh <alexkorzh7@gmail.com>
2024-08-06 00:56:43 -0700

    Update main layout styles

--- machobear-test-fe/src/app/layout.tsx
+++ machobear-test-fe/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Work_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-red-500 font-sans antialiased",
+          workSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
